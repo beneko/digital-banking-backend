cc6fd7705913293bccfbbce0bba907207b70339c
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-21 14:54:46 +0200

    first commit

---
+++ src/main/java/com/digital/banking/DigitalBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digital.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/banking/DigitalBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
