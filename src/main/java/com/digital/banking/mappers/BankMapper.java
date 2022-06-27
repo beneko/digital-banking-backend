48c173ce9de5cb286b28409c28f7e88b82323939
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-27 11:06:44 +0200

    BankMapper interface and impl created

--- src/main/java/com/digital/banking/mappers/BankMapper.java
+++ src/main/java/com/digital/banking/mappers/BankMapper.java
@@ -1,2 +1,9 @@
-package com.digital.banking.mappers;public interface BankMapper {
+package com.digital.banking.mappers;
+
+import com.digital.banking.dtos.CustomerDTO;
+import com.digital.banking.entities.Customer;
+
+public interface BankMapper {
+    CustomerDTO fromCustomer(Customer customer);
+    Customer fromCustomerDTO(CustomerDTO customerDTO);
 }
--- src/main/java/com/digital/banking/mappers/BankMapperImpl.java
+++ src/main/java/com/digital/banking/mappers/BankMapperImpl.java
@@ -1,2 +1,23 @@
-package com.digital.banking.mappers;public class BankMapperImpl {
+package com.digital.banking.mappers;
+
+import com.digital.banking.dtos.CustomerDTO;
+import com.digital.banking.entities.Customer;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+//We can also use MapStruct
+@Service
+public class BankMapperImpl implements BankMapper{
+    @Override
+    public CustomerDTO fromCustomer(Customer customer){
+        CustomerDTO customerDTO = new CustomerDTO();
+        BeanUtils.copyProperties(customer, customerDTO);
+        return customerDTO;
+    }
+    @Override
+    public Customer fromCustomerDTO(CustomerDTO customerDTO){
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDTO, customer);
+        return customer;
+    }
 }
