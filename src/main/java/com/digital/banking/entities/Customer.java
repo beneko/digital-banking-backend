5be175b68cc404b47296a7558657204b62eae9b1
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-27 11:29:21 +0200

    put JsonProperty annotation in comment

--- src/main/java/com/digital/banking/entities/Customer.java
+++ src/main/java/com/digital/banking/entities/Customer.java
@@ -16,6 +16,6 @@ public class Customer {
     private String name;
     private String email;
     @OneToMany(mappedBy = "customer")
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    //@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<BankAccount> bankAccountsList;
 }
