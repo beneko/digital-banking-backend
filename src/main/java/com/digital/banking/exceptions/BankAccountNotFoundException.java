4a75af141dad254cf444d491a7689a15503555df
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-28 16:30:13 +0200

    add another constructor with message

--- src/main/java/com/digital/banking/exceptions/BankAccountNotFoundException.java
+++ src/main/java/com/digital/banking/exceptions/BankAccountNotFoundException.java
@@ -1,6 +1,10 @@
 package com.digital.banking.exceptions;
 
 public class BankAccountNotFoundException extends Exception{
+    public BankAccountNotFoundException(String message) {
+        super(message);
+    }
+
     public BankAccountNotFoundException() {
         super("Bank account not found!");
     }
