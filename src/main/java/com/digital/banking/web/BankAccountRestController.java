15cca3824dc86c02e1ccddc326fe7d89c1e6ca83
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-28 11:35:07 +0200

    add method to get account history with pagination in restController

--- src/main/java/com/digital/banking/web/BankAccountRestController.java
+++ src/main/java/com/digital/banking/web/BankAccountRestController.java
@@ -1,15 +1,13 @@
 package com.digital.banking.web;
 
+import com.digital.banking.dtos.AccountHistoryDTO;
 import com.digital.banking.dtos.BankAccountDTO;
 import com.digital.banking.dtos.OperationDTO;
 import com.digital.banking.exceptions.BankAccountNotFoundException;
 import com.digital.banking.services.BankService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +29,11 @@ public class BankAccountRestController {
     @GetMapping("/accounts/{accountId}/operations")
     public List<OperationDTO> getAccountOperationHistory(@PathVariable String accountId){
         return bankService.getOperationList(accountId);
+    }
+    @GetMapping("/accounts/{accountId}/pageOperation")
+    public AccountHistoryDTO getAccountHistory(@PathVariable String accountId,
+                                               @RequestParam(name = "offset", defaultValue = "0") int offset,
+                                               @RequestParam(name = "limit", defaultValue = "10") int limit) throws BankAccountNotFoundException {
+        return bankService.getAccountHistory(accountId, offset, limit);
     }
 }
