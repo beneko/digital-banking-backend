08efdb3b077e1b79a9b57ad57b68708ec42318c6
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-28 15:10:48 +0200

    WebRestControllerAdvice created and resourceNotFoundExceptionHandler method impelemented

---
+++ src/main/java/com/digital/banking/advices/WebRestControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.digital.banking.advices;
+
+import com.digital.banking.exceptions.BankAccountNotFoundException;
+import com.digital.banking.exceptions.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+@RestControllerAdvice
+public class WebRestControllerAdvice {
+
+    @ExceptionHandler({BankAccountNotFoundException.class, CustomerNotFoundException.class, AccountNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String resourceNotFoundExceptionHandler(Exception ex) {
+        return ex.getMessage();
+    }
+}
