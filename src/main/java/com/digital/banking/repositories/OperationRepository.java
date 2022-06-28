278844ae294785bb12eaf7d12668bbe010004c8f
Behzad NEKOUEI <behzadnekouei@gmail.com>
2022-06-28 11:31:56 +0200

    create a new method in OperationRepository

--- src/main/java/com/digital/banking/repositories/OperationRepository.java
+++ src/main/java/com/digital/banking/repositories/OperationRepository.java
@@ -1,10 +1,13 @@
 package com.digital.banking.repositories;
 
 import com.digital.banking.entities.Operation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OperationRepository extends JpaRepository<Operation, Long> {
     List<Operation> findByBankAccountId(String bankAccountId);
+    Page<Operation> findByBankAccountId(String bankAccountId, Pageable pageable);
 }
